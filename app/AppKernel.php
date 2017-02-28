8350a967717aed79e5e9ceece92c5095c31ad71d
mariuszpasieczny <mpasieczny@gmail.com>
2017-02-28 15:28:00 +0100

    initial commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,11 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
+    public function init() {
+        bcscale(3);
+    }
+    
     public function registerBundles()
     {
         $bundles = array(
@@ -29,6 +34,35 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+            
+            new Sonata\ClassificationBundle\SonataClassificationBundle(),
+            new Application\Sonata\ClassificationBundle\ApplicationSonataClassificationBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Sonata\MediaBundle\SonataMediaBundle(),
+            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+            new Sonata\NotificationBundle\SonataNotificationBundle(),
+            new Application\Sonata\NotificationBundle\ApplicationSonataNotificationBundle(),
+            
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Sonata\FormatterBundle\SonataFormatterBundle(),
+            new Sonata\IntlBundle\SonataIntlBundle(),
+            
+            new Sonata\CustomerBundle\SonataCustomerBundle(),
+            new Sonata\ProductBundle\SonataProductBundle(),
+            new Sonata\BasketBundle\SonataBasketBundle(),
+            new Sonata\OrderBundle\SonataOrderBundle(),
+            new Sonata\InvoiceBundle\SonataInvoiceBundle(),
+            new Sonata\DeliveryBundle\SonataDeliveryBundle(),
+            new Sonata\PaymentBundle\SonataPaymentBundle(),
+            new Sonata\PriceBundle\SonataPriceBundle(),
+            new Application\Sonata\CustomerBundle\ApplicationSonataCustomerBundle(),
+            new Application\Sonata\DeliveryBundle\ApplicationSonataDeliveryBundle(),
+            new Application\Sonata\BasketBundle\ApplicationSonataBasketBundle(),
+            new Application\Sonata\InvoiceBundle\ApplicationSonataInvoiceBundle(),
+            new Application\Sonata\OrderBundle\ApplicationSonataOrderBundle(),
+            new Application\Sonata\PaymentBundle\ApplicationSonataPaymentBundle(),
+            new Application\Sonata\ProductBundle\ApplicationSonataProductBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
